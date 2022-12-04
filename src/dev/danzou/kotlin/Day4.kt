fbe0cb92daa767a7e1093e4a093e86e6c81d7e49
Dan Zou <danzou193@gmail.com>
2022-12-04 00:39:31 -0500

    Implement kotlin day 4 solution

---
+++ src/dev/danzou/kotlin/Day4.kt
@@ -0,0 +1,26 @@
+package dev.danzou.kotlin
+
+import dev.danzou.kotlin.utils.AdventTestRunner
+
+class Day4 : AdventTestRunner() {
+    override fun part1(input: String): Number {
+        return input.split("\n").map {
+            it.split(",", "-").map(String::toInt)
+        }.map {
+            Pair(it[0]..it[1], it[2]..it[3])
+        }.count { (range1, range2) ->
+            range1.first <= range2.first && range2.last <= range1.last ||
+                    range2.first <= range1.first && range1.last <= range2.last
+        }
+    }
+
+    override fun part2(input: String): Number {
+        return input.split("\n").map {
+            it.split(",", "-").map(String::toInt)
+        }.map {
+            Pair(it[0]..it[1], it[2]..it[3])
+        }.count {
+            (it.first intersect it.second).isNotEmpty()
+        }
+    }
+}
