dd0c1870e286cd642e6f0a3f23f7f13ef399f891
Dan Zou <danzou193@gmail.com>
2022-12-09 02:03:28 -0500

    Clean up

--- src/main/dev/danzou/advent22/kotlin/Day9.kt
+++ src/main/dev/danzou/advent22/kotlin/Day9.kt
@@ -10,20 +10,16 @@ import kotlin.math.absoluteValue
 
 internal class Day9 : AdventTestRunner() {
 
-    fun simulate(rope: Rope, dir: Pair<Int, Int>): Rope {
-        val newHead = rope.head + dir
-        val oldTail = rope.tail
-        val diff = newHead - oldTail
-
-        val offset = when {
-            (diff.toList().map { it.absoluteValue }.max() > 1) -> (newHead - oldTail).toList()
-                .map {
-                    if (it == 0) it
-                    else it.absoluteValue / it
-                }.toPair()
-            else -> Pair(0, 0)
-        }
-        return Rope(newHead, oldTail + offset)
+    fun simulate(newHead: Pair<Int, Int>, oldTail: Pair<Int, Int>): Rope =
+        (newHead - oldTail).toList().let { diffAsList ->
+            Rope(newHead, oldTail + when {
+                diffAsList.maxOf { it.absoluteValue } > 1 ->
+                    diffAsList.map {
+                        if (it == 0) it
+                        else it.absoluteValue / it
+                    }.toPair()
+                else -> Pair(0, 0)
+            })
     }
 
     data class Rope(val head: Pair<Int, Int> = Pair(0, 0), val tail: Pair<Int, Int> = Pair(0, 0))
@@ -45,20 +41,12 @@ internal class Day9 : AdventTestRunner() {
         val res = parse(input)
             .fold(Pair(Rope(), emptySet<Pair<Int, Int>>())) { (rope, visited), (dir, amt) ->
                 (0 until amt).fold(Pair(rope, visited)) { (rope, visited), _ ->
-                    simulate(rope, dir).let { newRope ->
+                    simulate(rope.head + dir, rope.tail).let { newRope ->
                         Pair(newRope, visited + newRope.tail)
                     }
-                }.also { (rope, _) -> /*println(rope.head)*/ }
+                }
             }
         return res.second.size
-    }
-
-    fun simulate(newHead: Pair<Int, Int>, oldTail: Pair<Int, Int>): Rope {
-        return Rope(newHead, oldTail + (newHead - oldTail).toList()
-            .map {
-                if (it == 0) it
-                else it.absoluteValue / it
-            }.toPair())
     }
 
     override fun part2(input: String): Any {
@@ -68,34 +56,13 @@ internal class Day9 : AdventTestRunner() {
                 emptySet<Pair<Int, Int>>()
             )) { (ropes, visited), (dir, amt) ->
                 (0 until amt).fold(Pair(ropes, visited)) { (ropes, visited), _ ->
-                    ropes.foldIndexed(
-                        Pair(emptyList<Rope>(), dir)
-                    ) { i, (ropesAcc, dir), rope ->
-                        when (dir) {
-                            Pair(0, 0) -> Pair(ropesAcc + rope, rope.tail - ropes.getOrElse(i + 1) { ropes[i] }.head)
-                            else -> simulate(rope, dir).let { newRope ->
-                                Pair(ropesAcc + newRope, newRope.tail - ropes.getOrElse(i + 1, { ropes[i] }).head)
-                            }
-                        }
-                    }.let { (newRopes, _) ->
-                        Pair(newRopes, visited + newRopes.last().tail).also {
-//                            println(it.first)
-                        }
-                    }
-/*                    ropes.drop(1).fold(
-                        listOf(simulate(ropes.first(), dir))
+                    ropes.drop(1).fold(
+                        listOf(simulate(ropes.first().head + dir, ropes.first().tail))
                     ) { ropes, rope ->
-                        (rope.head - ropes.last().tail).let { diff ->
-                            when (diff) {
-                                Pair(0, 0) -> ropes + rope
-                                else -> ropes + simulate(rope, diff)
-                            }
-                        }
+                        ropes + simulate(ropes.last().tail, rope.tail)
                     }.let { newRopes ->
                         Pair(newRopes, visited + newRopes.last().tail)
-                    }*/
-                }.also {
-//                    println(it.first)
+                    }
                 }
             }
         return res.second.size
