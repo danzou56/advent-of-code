d1465c54ddda7e5831e9570d1ee3be0e55f7ac5f
Dan Zou <danzou193@gmail.com>
2022-12-12 01:01:13 -0500

    Cleanup day 12

--- src/main/dev/danzou/advent22/kotlin/Day12.kt
+++ src/main/dev/danzou/advent22/kotlin/Day12.kt
@@ -5,23 +5,31 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal class Day12 : AdventTestRunner() {
-    override fun part1(input: String): Any {
-        val matrix: Matrix<Char> = input.split("\n").map { it.split("").filter { it.isNotEmpty() }.map { it.first() } }
-        val start: Pos = matrix.indexOfFirst { it.contains('S') }.let { i -> Pair(i, matrix[i].indexOfFirst { it == 'S' }) }
-        val end: Pos = matrix.indexOfFirst { it.contains('E') }.let { i -> Pair(i, matrix[i].indexOfFirst { it == 'E' }) }
 
-        val mapper = { c: Char -> when (c) {
-            'S' -> 'a'
-            'E' -> 'z'
-            else -> c
-        } }
+    fun parse(input: String): Triple<Matrix<Char>, Pos, Pos> =
+        input.split("\n").map { it.toList() }.let { matrix ->
+            Triple(
+                matrix.map2D { c ->
+                    when (c) {
+                        'S' -> 'a'
+                        'E' -> 'z'
+                        else -> c
+                    }
+                },
+                matrix.indexOfFirst { it.contains('S') }.let { i -> Pair(i, matrix[i].indexOf('S')) },
+                matrix.indexOfFirst { it.contains('E') }.let { i -> Pair(i, matrix[i].indexOf('E')) },
+            )
+        }
+
+    override fun part1(input: String): Any {
+        val (matrix, start, end) = parse(input)
 
         val path = doDijkstras(
             start,
             end,
             { cur ->
                 matrix.getNeighboringPos(cur.first, cur.second).filter { next ->
-                    mapper(matrix[next]) <= mapper(matrix[cur]) + 1
+                    matrix[next] <= matrix[cur] + 1
                 }.toSet()
             },
             { _, _ -> 1 }
@@ -31,28 +39,22 @@ internal class Day12 : AdventTestRunner() {
     }
 
     override fun part2(input: String): Any {
-        val matrix: Matrix<Char> = input.split("\n").map { it.split("").filter { it.isNotEmpty() }.map { it.first() } }
-        val end: Pos = matrix.indexOfFirst { it.contains('E') }.let { i -> Pair(i, matrix[i].indexOfFirst { it == 'E' }) }
+        val (matrix, _, end) = parse(input)
 
-        val mapper = { c: Char -> when (c) {
-            'S' -> 'a'
-            'E' -> 'z'
-            else -> c
-        } }
+        val paths = matrix.indices2D.filter { p -> matrix[p] == 'a' }.map { start ->
+            doDijkstras(
+                start,
+                end,
+                { cur ->
+                    matrix.getNeighboringPos(cur.first, cur.second).filter { next ->
+                        matrix[next] <= matrix[cur] + 1
+                    }.toSet()
+                },
+                { _, _ -> 1 }
+            )
+        }
 
-        val paths = matrix.indices2D.filter { p -> matrix[p] == 'a' || matrix[p] == 'S' }.map { start -> doDijkstras(
-            start,
-            end,
-            { cur ->
-                matrix.getNeighboringPos(cur.first, cur.second).filter { next ->
-                    mapper(matrix[next]) <= mapper(matrix[cur]) + 1
-                }.toSet()
-            },
-            { _, _ -> 1 }
-        ) }
-
-        val res = paths.filter { it.isNotEmpty() }.minBy { it.size }.size - 1
-        return res
+        return paths.filter { it.isNotEmpty() }.minBy { it.size }.size - 1
     }
 
     @Test
