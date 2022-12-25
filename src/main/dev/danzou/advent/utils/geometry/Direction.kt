8651d10289394a59d6bcc80d6b50e5fe04f40c3b
Dan Zou <danzou193@gmail.com>
2022-12-24 19:39:21 -0500

    Add compass directions

--- src/main/dev/danzou/advent/utils/geometry/Direction.kt
+++ src/main/dev/danzou/advent/utils/geometry/Direction.kt
@@ -8,3 +8,25 @@ enum class Direction(val dir: Pos) {
     DOWN(Pos(0, -1)),
     UP(Pos(0, 1));
 }
+
+enum class Compass(val dir: Pos) {
+    NORTH(Pos(0, -1)),
+    EAST(Pos(1, 0)),
+    SOUTH(Pos(0, 1)),
+    WEST(Pos(-1, 0)),
+    NORTHEAST(Pos(1, -1)),
+    SOUTHEAST(Pos(1, 1)),
+    SOUTHWEST(Pos(-1, 1)),
+    NORTHWEST(Pos(-1, -1));
+
+    companion object {
+        fun cardinalDirections(): Set<Compass> =
+            setOf(NORTH, EAST, SOUTH, WEST)
+
+        fun ordinalDirections(): Set<Compass> =
+            setOf(NORTHEAST, SOUTHEAST, SOUTHWEST, NORTHWEST)
+
+        fun directions(): Set<Compass> =
+            values().toSet()
+    }
+}
