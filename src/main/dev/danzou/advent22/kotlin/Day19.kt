a368b704a4124294117cd7f931589933a3704788
Dan Zou <danzou193@gmail.com>
2023-01-12 16:52:23 -0500

    Add stub day 16/19 kotlin files

---
+++ src/main/dev/danzou/advent22/kotlin/Day19.kt
@@ -0,0 +1,13 @@
+package dev.danzou.advent22.kotlin
+
+import dev.danzou.advent.utils.AdventTestRunner
+
+internal class Day19 : AdventTestRunner() {
+    override fun part1(input: String): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun part2(input: String): Any {
+        TODO("Not yet implemented")
+    }
+}
