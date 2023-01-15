de41f26f77e4bc43bcbf15a20dd52c17c5448b0a
Dan Zou <danzou193@gmail.com>
2023-01-14 20:10:15 -0500

    Remove unimplemented scala day 2

--- src/main/dev/danzou/advent22/scala/Day2.scala
+++
@@ -1,9 +0,0 @@
-package dev.danzou.advent22.scala
-
-import dev.danzou.advent.utils.AdventTestRunner
-
-class Day2 extends AdventTestRunner {
-  override def part1(inputString: String): Number = ???
-
-  override def part2(inputString: String): Number = ???
-}
