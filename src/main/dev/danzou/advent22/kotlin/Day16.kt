dbbbb11366ee650b8a5a96bd063ea9bbbf004ba6
Dan Zou <danzou193@gmail.com>
2023-01-21 20:46:56 -0500

    Clean up day 16 part 1

--- src/main/dev/danzou/advent22/kotlin/Day16.kt
+++ src/main/dev/danzou/advent22/kotlin/Day16.kt
@@ -20,7 +20,7 @@ internal class Day16 : AdventTestRunner22() {
             // and one fewer number at the back. This is because the pressure
             // released by opening a valve isn't produced until the **next**
             // step and not the current one
-            return path.windowed(2).also { assert(it.size == 30) }.fold(Pair(listOf(0), emptySet<Valve>())) { (pressures, opened), (prev, next) ->
+            return path.windowed(2).fold(Pair(listOf(0), emptySet<Valve>())) { (pressures, opened), (prev, next) ->
                 if (prev == next && prev !in opened) Pair(pressures + (pressures.last() + valves[prev]!!), opened + prev)
                 else Pair(pressures + pressures.last(), opened)
             }.first.dropLast(1).sum()
@@ -48,34 +48,25 @@ internal class Day16 : AdventTestRunner22() {
         val costCeiling = volcano.valves.map { limit * it.value }.max()
         val openable = volcano.valves.filter { it.value > 0 }.keys
 
+        data class State(val time: Int, val valve: Valve, val opened: Set<Valve>)
+
         val path = doDijkstras(
-            init = Triple(0, Valve("AA"), emptySet<Valve>()),
+            init = State(0, Valve("AA"), emptySet<Valve>()),
             target = { (t, _, opened) -> t >= limit || opened == openable },
             getNeighbors = { (t, valve, opened) ->
-                when {
-                    valve !in opened && volcano.valves[valve]!! > 0 ->
-                        setOf(Triple(t + 1, valve, opened + valve))
-                    else ->
-                        volcano.tunnels[valve]!!.map { Triple(t + 1, it, opened) }.toSet()
-                }
+                if (valve !in opened && volcano.valves[valve]!! > 0)
+                        setOf(State(t + 1, valve, opened + valve))
+                else volcano.tunnels[valve]!!.map { State(t + 1, it, opened) }.toSet()
             },
-            getCost = { (t, valve0, opened0), (t1, valve1, opened1) ->
-                require(t + 1 == t1)
-                when {
-                    valve0 == valve1 && valve0 !in opened0 && valve1 in opened1 ->
-                        costCeiling - volcano.valves[valve0]!! * (limit - t)
-                    else -> {
-                        require(
-                            valve0 in opened0 && volcano.valves[valve0]!! > 0 ||
-                            valve0 !in opened0 && volcano.valves[valve0]!! == 0
-                        )
-                        costCeiling
-                    }
-                }
+            getCost = { prev, next ->
+                require(prev.time + 1 == next.time)
+                if (prev.valve == next.valve)
+                    costCeiling - volcano.valves[prev.valve]!! * (limit - prev.time)
+                else costCeiling
             }
         )
 
-        val valves = path.map { it.second }
+        val valves = path.map { it.valve }
         assert(valves.isNotEmpty())
         return volcano.pressureReleasedFrom(
             (valves + List(limit - valves.size + 1) { valves.last() })
@@ -103,38 +94,8 @@ internal class Day16 : AdventTestRunner22() {
 
         val volcano = Volcano.fromString(input)
         val expectedPath = """
-            AA
-            DD
-            DD
-            CC
-            BB
-            BB
-            AA
-            II
-            JJ
-            JJ
-            II
-            AA
-            DD
-            EE
-            FF
-            GG
-            HH
-            HH
-            GG
-            FF
-            EE
-            EE
-            DD
-            CC
-            CC
-            CC
-            CC
-            CC
-            CC
-            CC
-            CC
-        """.trimIndent().split("\n").map { Valve(it) }
+            AA,DD,DD,CC,BB,BB,AA,II,JJ,JJ,II,AA,DD,EE,FF,GG,HH,HH,GG,FF,EE,EE,DD,CC,CC,CC,CC,CC,CC,CC,CC
+        """.trimIndent().split(",").map { Valve(it) }
 
         assertEquals(31, expectedPath.size)
         assertEquals(1651, volcano.pressureReleasedFrom(expectedPath))
