99778edd872e43cec722b84eca54c57d5c7e072f
Dan Zou <danzou193@gmail.com>
2023-11-03 14:59:10 -0400

    Implement 2021 20

--- src/main/dev/danzou/advent/utils/Utils.kt
+++ src/main/dev/danzou/advent/utils/Utils.kt
@@ -9,3 +9,5 @@ import java.security.MessageDigest
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
 
 fun <T> List<T>.update(index: Int, item: T): List<T> = toMutableList().apply { this[index] = item }
+
+fun <T> Int.times(initial: T, operation: (T) -> T): T = (0 until this).fold(initial) { acc, _ -> operation(acc) }
