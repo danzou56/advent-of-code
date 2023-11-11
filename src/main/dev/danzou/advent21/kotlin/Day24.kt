bbb00f9dc366c0100211329eef97ecea8f19f647
Dan Zou <danzou193@gmail.com>
2023-11-11 13:46:53 -0500

    Clean up extra, unneeded classes/func 2021 24

--- src/main/dev/danzou/advent21/kotlin/Day24.kt
+++ src/main/dev/danzou/advent21/kotlin/Day24.kt
@@ -7,44 +7,22 @@ import java.util.*
 
 class Day24 : AdventTestRunner21() {
     data class AluState(val store: Map<Char, Long>) {
-        constructor() : this(emptyMap())
+        constructor() : this(mapOf('w' to 0, 'x' to 0, 'y' to 0, 'z' to 0))
 
-        fun advance(instruction: Instruction): AluState =
-            this.copy(
+        inline fun advance(op: (Long, Long) -> Long, target: String, operand: String): AluState {
+            return this.copy(
                 store = this.store + mapOf(
-                    instruction.target.identifier to instruction.op(
-                        store[instruction.target.identifier] ?: 0,
-                        when (instruction.operand) {
-                            is Operand.Identifier -> store[instruction.operand.identifier] ?: 0
-                            is Operand.Literal -> instruction.operand.number
+                    target.first() to op(
+                        store[target.first()]!!,
+                        when {
+                            operand.first().isLetter() -> store[operand.first()]!!
+                            else -> operand.toLong()
                         }
                     )
                 )
             )
-    }
-
-    sealed class Operand {
-        data class Identifier(val identifier: Char) : Operand()
-        data class Literal(val number: Long) : Operand()
-    }
-
-    sealed class Instruction(val op: (Long, Long) -> Long, val target: Operand.Identifier, val operand: Operand) {
-        constructor(op: (Long, Long) -> Long, target: String, operand: String) : this(
-            op,
-            Operand.Identifier(target.first()),
-            when {
-                operand.first().isLetter() -> Operand.Identifier(operand.first())
-                else -> Operand.Literal(operand.toLong())
-            }
-        )
-
-        class Inp(target: String, inputNumber: Int) : Instruction({ _, _ -> inputNumber.toLong() }, target, target)
-        class Add(target: String, operand: String) : Instruction(Long::plus, target, operand)
-        class Mul(target: String, operand: String) : Instruction(Long::times, target, operand)
-        class Div(target: String, operand: String) : Instruction(Long::div, target, operand)
-        class Mod(target: String, operand: String) : Instruction(Long::mod, target, operand)
-        class Eql(target: String, operand: String) :
-            Instruction({ i1, i2 -> if (i1 == i2) 1L else 0L }, target, operand)
+        }
+            
     }
 
     override fun part1(input: String): Any {
@@ -59,23 +37,23 @@ class Day24 : AdventTestRunner21() {
             val rest = index + 1
             val (instr, target, operand) = (lines[index] + " ").split(" ")
 
-
-
             val nextAlu = aluState.advance(when (instr) {
-                "add" -> Instruction.Add(target, operand)
-                "mul" -> Instruction.Mul(target, operand)
-                "div" -> Instruction.Div(target, operand)
-                "mod" -> Instruction.Mod(target, operand)
-                "eql" -> Instruction.Eql(target, operand)
+                "add" -> Long::plus
+                "mul" -> Long::times
+                "div" -> Long::div
+                "mod" -> { l1: Long, l2: Long -> l1.mod(l2) }
+                "eql" -> { l1: Long, l2: Long -> if (l1 == l2) 1L else 0L }
                 else -> {
                     require(instr == "inp")
                     return (9 downTo 1).firstNotNullOfOrNull {
                         if (depth < 3) println("trying $it at depth $depth")
                         if (depth < 2) cache.clear()
-                        val nextAlu = aluState.advance(Instruction.Inp(target, it))
-//                        if (cache.containsKey(Pair(rest, nextAlu))) println("Cache hit")
+                        val nextAlu = aluState.advance(
+                            { _, l2 -> l2 },
+                            target,
+                            it.toString()
+                        )
                         val tail = cache.getOrPut(Pair(rest, nextAlu)) {
-//                            if (cache.size % 1000 == 0) println(cache.size)
                             genMonadNumber(rest, nextAlu, depth + 1)
                         }
                         if (tail.isPresent()) listOf(it) + tail.get()
@@ -85,34 +63,9 @@ class Day24 : AdventTestRunner21() {
                         else -> Optional.of(it)
                     } }
                 }
-            })
+            }, target, operand)
 
             return genMonadNumber(rest, nextAlu, depth)
-
-/*            return lines.first().split(" ").let { instr ->
-                when (instr.first()) {
-                    "inp" -> (9 downTo 1).firstNotNullOfOrNull {
-                        val tail = genMonadNumber(
-                            lines.drop(1),
-                            aluState.advance(Instruction.Inp(instr.component2(), it))
-                        )
-                        if (tail.isPresent()) listOf(it) + tail.get()
-                        else null
-                    }.let {
-                        when (it) {
-                            null -> Optional.empty()
-                            else -> Optional.of(it)
-                        }
-                    }
-
-                    "add" -> genMonadNumber(lines.drop(1), aluState.advance(Instruction.Add(instr[1], instr[2])))
-                    "mul" -> genMonadNumber(lines.drop(1), aluState.advance(Instruction.Mul(instr[1], instr[2])))
-                    "div" -> genMonadNumber(lines.drop(1), aluState.advance(Instruction.Div(instr[1], instr[2])))
-                    "mod" -> genMonadNumber(lines.drop(1), aluState.advance(Instruction.Mod(instr[1], instr[2])))
-                    "eql" -> genMonadNumber(lines.drop(1), aluState.advance(Instruction.Eql(instr[1], instr[2])))
-                    else -> TODO()
-                }
-            }*/
         }
 
         return genMonadNumber(
