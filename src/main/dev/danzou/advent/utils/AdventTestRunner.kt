274d0d7e1d158765659a64a096755aa42699aadc
Dan Zou <danzou193@gmail.com>
2023-11-20 20:28:43 -0500

    Move input files out of resources

--- src/main/dev/danzou/advent/utils/AdventTestRunner.kt
+++ src/main/dev/danzou/advent/utils/AdventTestRunner.kt
@@ -3,11 +3,16 @@ package dev.danzou.advent.utils
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertTimeoutPreemptively
 import java.io.IOException
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
 import java.nio.file.Files
 import java.nio.file.Path
 import java.time.Duration
 import kotlin.io.path.createFile
 import kotlin.io.path.notExists
+import kotlin.io.path.writeText
 import kotlin.test.assertEquals
 
 abstract class AdventTestRunner(protected val year: Int, protected val name: String? = null) {
@@ -15,14 +20,15 @@ abstract class AdventTestRunner(protected val year: Int, protected val name: Str
         get() = this.javaClass.simpleName.drop(3).takeWhile(Char::isDigit).toInt()
 
     protected open val timeout: Duration = Duration.ofSeconds(60)
-    private val dataRoot = "src/main/resources/dev/danzou"
+    private val dataRoot = "data/advent"
     private val basePath = "$dataRoot/advent$year"
     protected val baseInputPath = "$basePath/inputs"
     protected val baseOutputPath = "$basePath/outputs"
 
     private val input: String = readFileString(
-        "$baseInputPath/day$day.in"
-    )
+        "$baseInputPath/day$day.in",
+        "$AOC_BASE_URL/20$year/day/$day/input"
+    ).also { require(it.isNotEmpty()) }
 
     private val expected: List<String?> = readFileLines(
         "$baseOutputPath/day$day.out"
@@ -50,17 +56,35 @@ abstract class AdventTestRunner(protected val year: Int, protected val name: Str
     }
 
     companion object {
-        fun readFileLines(name: String): List<String> {
+        const val AOC_TOKEN_KEY = "AOC_TOKEN"
+        const val AOC_BASE_URL = "https://adventofcode.com"
+
+        fun readFileLines(name: String, fallbackUrl: String? = null): List<String> {
             return try {
                 val path = Path.of(name)
                 if (path.notExists()) {
                     println("No such file $name; creating empty file")
                     path.createFile()
                 }
+
                 val lines = Files.readAllLines(path)
-                if (lines.isEmpty() || lines[0].isEmpty())
-                    throw IOException("Empty file!")
-                else lines
+                if (lines.isNotEmpty() && lines[0].isNotEmpty()) return lines
+                if (fallbackUrl == null) throw IOException("Empty file")
+                return DotEnv[AOC_TOKEN_KEY]?.let { token ->
+                    val httpRequest = HttpRequest.newBuilder()
+                        .uri(URI(fallbackUrl))
+                        .headers("Cookie", "session=$token")
+                        .GET()
+                        .build()
+                    val client = HttpClient.newHttpClient()
+                    val response = client.send(httpRequest, HttpResponse.BodyHandlers.ofString())
+                    if (response.statusCode() == 200) {
+                        path.writeText(response.body())
+                        println("Retrieved AOC input and wrote into input file")
+                        return@let Files.readAllLines(path)
+                    }
+                    throw IOException("AOC fallback URL $fallbackUrl responded with code ${response.statusCode()}")
+                } ?: throw IllegalArgumentException("Fallback URL provided but no token")
             } catch (e: IOException) {
                 e.printStackTrace()
                 println("occurred while reading $name; returning empty list instead")
@@ -68,7 +92,8 @@ abstract class AdventTestRunner(protected val year: Int, protected val name: Str
             }
         }
 
-        fun readFileString(name: String): String = readFileLines(name).joinToString("\n")
+        fun readFileString(name: String, fallbackUrl: String? = null): String =
+            readFileLines(name, fallbackUrl).joinToString("\n")
 
         fun assertEqualAnswer(expected: String?, actual: Any?) {
             when (actual) {
