e0f771c99e9cdf5223bda600dbf051041f739530
Dan Zou <danzou193@gmail.com>
2023-11-30 14:35:50 -0500

    Get ready for 2023

---
+++ src/main/dev/danzou/advent23/kotlin/Day1.kt
@@ -0,0 +1,13 @@
+package dev.danzou.advent23.kotlin
+
+import dev.danzou.advent23.AdventTestRunner23
+
+internal class Day1 : AdventTestRunner23() {
+    override fun part1(input: String): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun part2(input: String): Any {
+        TODO("Not yet implemented")
+    }
+}
