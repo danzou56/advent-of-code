bff9d9f8381602ca757261b7eaa6b7a618a92da2
Dan Zou <danzou193@gmail.com>
2023-12-01 00:28:53 -0500

    Clean up implementation

--- src/main/dev/danzou/advent23/kotlin/Day1.kt
+++ src/main/dev/danzou/advent23/kotlin/Day1.kt
@@ -5,39 +5,26 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal class Day1 : AdventTestRunner23() {
-    override fun part1(input: String): Any {
-        return input.split("\n")
-            .sumOf { (it.find { it.isDigit() }.toString() + it.findLast { it.isDigit() }).toInt()}
-    }
-
-    val nums = """
-        one, two, three, four, five, six, seven, eight, nine
-    """.trimIndent().split(", ").mapIndexed { i, s -> s to i + 1 }.toMap()
+    override fun part1(input: String): Any = input.split("\n")
+        .sumOf { "${it.find(Char::isDigit)}${it.findLast(Char::isDigit)}".toInt() }
 
     override fun part2(input: String): Any {
+        val nums = """
+            one, two, three, four, five, six, seven, eight, nine
+        """.trimIndent()
+            .split(", ")
+            .mapIndexed { i, v -> v to i + 1 }
+            .toMap() + (1..9).associateBy { it.toString() }
+
         return input.split("\n")
             .sumOf { line ->
-                val firstIndex = (line.indexOfAny(
-                nums.keys + nums.values.map { it.toString() }
-                ))
-                val firstFound: Int = (nums.keys + nums.values.map { it.toString() }).first { it ->
-                    try {line.substring(firstIndex, firstIndex + it.length) == it }
-                    catch (e: StringIndexOutOfBoundsException) { false }
-                }.let {
-                    if (it in nums.keys) nums[it]!!
-                    else it.toInt()
-                }
-                val lastIndex = (line.lastIndexOfAny(
-                    nums.keys + nums.values.map { it.toString() }
-                ))
-                val lastFound: Int = (nums.keys + nums.values.map { it.toString() }).first { it ->
-                    try {line.substring(lastIndex, lastIndex + it.length) == it }
-                    catch (e: StringIndexOutOfBoundsException) { false }
-                }.let {
-                    if (it in nums.keys) nums[it]!!
-                    else it.toInt()
-                }
-                (firstFound.toString() + lastFound.toString()).toInt()
+                val first = line.indexOfAny(nums.keys).let { index ->
+                    nums.keys.first { line.drop(index).startsWith(it) }
+                }.let { nums[it]!! }
+                val last = line.lastIndexOfAny(nums.keys).let { index ->
+                    nums.keys.first { line.drop(index).startsWith(it) }
+                }.let { nums[it]!! }
+                "$first$last".toInt()
             }
     }
 
