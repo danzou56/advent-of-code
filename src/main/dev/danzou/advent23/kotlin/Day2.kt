33e9fdda131adf81af0af3a49828c44e5cce7be1
Dan Zou <danzou193@gmail.com>
2023-12-02 00:53:56 -0500

    cleanup 2023 Day 2 even more

--- src/main/dev/danzou/advent23/kotlin/Day2.kt
+++ src/main/dev/danzou/advent23/kotlin/Day2.kt
@@ -2,30 +2,24 @@ package dev.danzou.advent23.kotlin
 
 import dev.danzou.advent23.AdventTestRunner23
 
-internal class Day2 : AdventTestRunner23() {
-    enum class Cube {
-        Blue, Red, Green
-    }
+internal class Day2 : AdventTestRunner23("Cube Conundrum") {
+    enum class Cube { Blue, Red, Green }
 
-    fun getGames(input: String): List<Pair<String, List<Map<Cube, Int>>>> =
+    fun getGames(input: String): Map<Int, List<Map<Cube, Int>>> =
         input.split("\n")
-            .map { it.replace(" ", "") }
-            .map { it.drop("Game ".length - 1) }
-            .map {
-                Pair(it.takeWhile { it.isDigit() }, it.split(":").drop(1).single())
-            }
-            .map { (id, rest) ->
-                id to rest.split(";").map {
-                    it.split(",").map {
-                        val color = it.dropWhile { it.isDigit() }
-                        val num = it.dropLast(color.length).toInt()
+            .map { it.drop("Game ".length) }
+            .map { it.split(": ") }
+            .associate { (id, rolls) ->
+                id.toInt() to rolls.split("; ").map { roll ->
+                    roll.split(", ").associate {
+                        val (num, color) = it.split(" ")
                         when (color) {
                             "blue" -> Cube.Blue
                             "red" -> Cube.Red
                             "green" -> Cube.Green
                             else -> throw IllegalArgumentException("Invalid color")
-                        } to num
-                    }.toMap()
+                        } to num.toInt()
+                    }
                 }
             }
 
@@ -38,12 +32,14 @@ internal class Day2 : AdventTestRunner23() {
         return getGames(input)
             .filter { (_, maps: List<Map<Cube, Int>>) ->
                 maps.all { map -> maxCubes.all { (color, max) -> (map[color] ?: 0) <= max } }
-            }.sumOf { (id, _) -> id.toInt() }
+            }.keys.sum()
     }
 
     override fun part2(input: String): Int =
-        getGames(input).sumOf { (_, maps: List<Map<Cube, Int>>) ->
-            Cube.entries.map { maps.maxOf { map -> map[it] ?: 0 } }.reduce(Int::times)
+        getGames(input).entries.sumOf { (_, rolls: List<Map<Cube, Int>>) ->
+            Cube.entries
+                .map { color -> rolls.maxOf { roll -> roll[color] ?: 0 } }
+                .reduce(Int::times)
         }
 
 }
