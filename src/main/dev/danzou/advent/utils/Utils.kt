eef08f7813d9d8ebd0d6bbd3f6615a0e08183629
Dan Zou <danzou193@gmail.com>
2023-12-04 00:29:56 -0500

    Add reversed utility func

--- src/main/dev/danzou/advent/utils/Utils.kt
+++ src/main/dev/danzou/advent/utils/Utils.kt
@@ -10,6 +10,8 @@ import kotlin.reflect.full.memberProperties
  */
 fun String.md5(): String = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16)
 
+fun <R, T> Pair<R, T>.reversed(): Pair<T, R> = Pair(this.second, this.first)
+
 fun <T> List<T>.update(index: Int, item: T): List<T> = toMutableList().apply { this[index] = item }
 
 fun <T> Int.times(initial: T, operation: (T) -> T): T = (0 until this).fold(initial) { acc, _ -> operation(acc) }
