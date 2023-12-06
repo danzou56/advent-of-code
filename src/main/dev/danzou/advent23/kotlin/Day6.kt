3c26eaffed5dd46007fabc4a5053579869b6ae91
Dan Zou <danzou193@gmail.com>
2023-12-06 00:44:28 -0500

    Implement 2023-6

---
+++ src/main/dev/danzou/advent23/kotlin/Day6.kt
@@ -0,0 +1,35 @@
+package dev.danzou.advent23.kotlin
+
+import dev.danzou.advent.utils.quadSolve
+import dev.danzou.advent23.AdventTestRunner23
+import kotlin.math.ceil
+import kotlin.math.floor
+
+internal class Day6 : AdventTestRunner23() {
+    // completely overbuilt for the problem because we can actually just brute force try all values
+    fun waysToWin(time: Long, distance: Long): Long {
+        fun f(t: Double): Double = t * (time - t)
+        // d := distance, t := button time, T := race duration
+        // solve quadratic t * (T - t) > d for unknown t
+        val solutions = quadSolve(-1.0, time.toDouble(), -distance.toDouble())
+        require(solutions.size == 2)
+
+        val max = solutions.max()
+        val min = solutions.min()
+        return 1 + (if (f(ceil(max)) > distance) ceil(max) else floor(max)).toLong() -
+                (if (f(floor(min)) > distance) floor(min) else ceil(min)).toLong()
+    }
+
+    override fun part1(input: String): Long {
+        val (times, distances) = input.split("\n")
+            .map { Regex("\\d+").findAll(it).map { it.value.toInt() }.toList() }
+        return times.zip(distances).map { (time, distance) -> waysToWin(time.toLong(), distance.toLong()) }
+            .reduce(Long::times)
+    }
+
+    override fun part2(input: String): Long {
+        val (time, distance) = input.split("\n")
+            .map { Regex("\\d+").findAll(it).map { it.value }.reduce(String::plus).toLong() }
+        return waysToWin(time, distance)
+    }
+}
