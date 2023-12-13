93eb67264156b4a1ce860342e7471c8eac053781
Dan Zou <danzou193@gmail.com>
2023-12-13 16:59:43 -0500

    Fix 2023-13 comment on list slices

--- src/main/dev/danzou/advent23/kotlin/Day13.kt
+++ src/main/dev/danzou/advent23/kotlin/Day13.kt
@@ -10,9 +10,9 @@ internal class Day13 : AdventTestRunner23("Point of Incidence") {
 
     private fun reflectionIndices(matrix: Matrix<Boolean>): Pair<Collection<Int>, Collection<Int>> {
         // It's much clearer what's going on when this logic is written as list slices, but kotlin
-        // list slices aren't particuarly fast since new lists are constantly being created. We get
-        // a 5x speed improvement just by switching colIndices calculation from list slices to list
-        // accesses by index (no substantial speedup is observed from making the refactor for
+        // list slices aren't particularly fast since new lists are constantly being created(?). We
+        // get a 5x speed improvement just by switching colIndices calculation from list slices to
+        // list accesses by index (no substantial speedup is observed from making the refactor for
         // rowIndices)
         val colIndices = (1..<matrix[0].size).filter { reflIndex ->
             val size = min(reflIndex, matrix[0].size - reflIndex)
