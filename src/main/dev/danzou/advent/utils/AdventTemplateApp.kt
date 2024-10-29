87f3f640bcd58a6b08bd90e8d141292dfd7b9dfe
Dan Zou <danzou193@gmail.com>
2024-10-28 22:59:02 -0400

    Refactor for arbitrary runs

--- src/main/dev/danzou/advent/utils/AdventTemplateApp.kt
+++ src/main/dev/danzou/advent/utils/AdventTemplateApp.kt
@@ -6,42 +6,59 @@ import java.time.Month
 import kotlin.io.path.Path
 import kotlin.io.path.writeText
 
+val adventRootPath = "src/main/dev/danzou"
+
 fun main() {
-    val tomorrow = LocalDate.now().plusDays(1)
-    val year = tomorrow.year.toString().takeLast(2)
-    if (tomorrow.month != Month.DECEMBER) throw RuntimeException("It's not December!")
-    val day = tomorrow.dayOfMonth
+  //    writeTomorrowsAocFile()
+  writeAocFile(20, 4)
+}
 
-    val adventRootPath = "src/main/dev/danzou"
-    val targetFilePath = Path("$adventRootPath/advent$year/kotlin/Day$day.kt")
+fun writeTomorrowsAocFile() {
+  val tomorrow = LocalDate.now().plusDays(1)
+  val year = tomorrow.year
+  if (tomorrow.month != Month.DECEMBER) throw RuntimeException("It's not December!")
+  val day = tomorrow.dayOfMonth
 
-    val fileText = """
-        package dev.danzou.advent$year.kotlin
+  writeAocFile(year.mod(100), day)
+}
 
-        import dev.danzou.advent.utils.*
-        import dev.danzou.advent$year.AdventTestRunner$year
-        import org.junit.jupiter.api.Assertions.assertEquals
-        import org.junit.jupiter.api.Test
-    
-        internal class Day$day : AdventTestRunner$year() {
+fun writeAocFile(year: Int, day: Int) {
+  require(year in 15..99)
+
+  val targetFilePath = Path("$adventRootPath/advent$year/kotlin/Day$day.kt")
+
+  val fileText =
+      """
+          package dev.danzou.advent$year.kotlin
+  
+          import dev.danzou.advent.utils.*
+          import dev.danzou.advent$year.AdventTestRunner$year
+          import org.junit.jupiter.api.Assertions.assertEquals
+          import org.junit.jupiter.api.Test
+      
+          internal class Day$day : AdventTestRunner$year("") {
             override fun part1(input: String): Any {
-                return input.split("\n")
+              TODO()
             }
     
             override fun part2(input: String): Any {
-                return input.split("\n")
+              TODO()
             }
             
             @Test
             fun testExample() {
-                val input = ""${'"'}
+              val input = 
+                  ""${'"'}
                     
-                ""${'"'}.trimIndent()
+                  ""${'"'}
+                      .trimIndent()
         
-                assertEquals(null, part1(input))
-        //        assertEquals(null, part2(input))
+              assertEquals(null, part1(input))
+              // assertEquals(null, part2(input))
             }
-        }
-    """.trimIndent()
-    targetFilePath.writeText(fileText, options = arrayOf(StandardOpenOption.CREATE_NEW))
+          }
+        """
+          .trimIndent()
+
+  targetFilePath.writeText(fileText, options = arrayOf(StandardOpenOption.CREATE_NEW))
 }
