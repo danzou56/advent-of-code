7da4495e597356ca81d11e46c53cef9ced8d367e
Dan Zou <danzou193@gmail.com>
2024-12-04 01:03:45 -0500

    Add 2024 day 4

---
+++ src/main/kotlin/dev/danzou/advent24/Day4.kt
@@ -0,0 +1,80 @@
+package dev.danzou.advent24
+
+import dev.danzou.advent.utils.*
+import dev.danzou.advent.utils.geometry.Compass
+import dev.danzou.advent.utils.geometry.plus
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class Day4 : AdventTestRunner24("") {
+
+  fun isXmas(mat: Matrix<Char>, start: Pos, dir: Compass): Boolean {
+    return mat[start] == 'X' &&
+        mat.containsPos(start + dir.dir) &&
+        mat[start + dir.dir] == 'M' &&
+        mat.containsPos(start + dir.dir + dir.dir) &&
+        mat[start + dir.dir + dir.dir] == 'A' &&
+        mat.containsPos(start + dir.dir + dir.dir + dir.dir) &&
+        mat[start + dir.dir + dir.dir + dir.dir] == 'S'
+  }
+
+  override fun part1(input: String): Number {
+    val mat = input.asMatrix<Char>()
+
+    val xs = mat.indices2D.filter { mat[it] == 'X' }
+    return xs.sumOf { xPos -> Compass.ALL.count { isXmas(mat, xPos, it) } }
+  }
+
+  fun isXmas2(mat: Matrix<Char>, start: Pos, dir: Compass): Boolean {
+    when (dir) {
+      Compass.EAST ->
+          return mat.getOrNull(start + Compass.NORTHEAST.dir) == 'M' &&
+              mat.getOrNull(start + Compass.SOUTHEAST.dir) == 'M' &&
+              mat.getOrNull(start + Compass.NORTHWEST.dir) == 'S' &&
+              mat.getOrNull(start + Compass.SOUTHWEST.dir) == 'S'
+      Compass.WEST ->
+          return mat.getOrNull(start + Compass.NORTHEAST.dir) == 'S' &&
+              mat.getOrNull(start + Compass.SOUTHEAST.dir) == 'S' &&
+              mat.getOrNull(start + Compass.NORTHWEST.dir) == 'M' &&
+              mat.getOrNull(start + Compass.SOUTHWEST.dir) == 'M'
+      Compass.SOUTH ->
+          return mat.getOrNull(start + Compass.NORTHEAST.dir) == 'S' &&
+              mat.getOrNull(start + Compass.SOUTHEAST.dir) == 'M' &&
+              mat.getOrNull(start + Compass.NORTHWEST.dir) == 'S' &&
+              mat.getOrNull(start + Compass.SOUTHWEST.dir) == 'M'
+      Compass.NORTH ->
+          return mat.getOrNull(start + Compass.NORTHEAST.dir) == 'M' &&
+              mat.getOrNull(start + Compass.SOUTHEAST.dir) == 'S' &&
+              mat.getOrNull(start + Compass.NORTHWEST.dir) == 'M' &&
+              mat.getOrNull(start + Compass.SOUTHWEST.dir) == 'S'
+      else -> return false
+    }
+  }
+
+  override fun part2(input: String): Number {
+    val mat = input.asMatrix<Char>()
+    val centers = mat.indices2D.filter { mat[it] == 'A' }
+    return centers.count { xPos -> Compass.ALL.any { isXmas2(mat, xPos, it) } }
+  }
+
+  @Test
+  fun testExample() {
+    """
+      MMMSXXMASM
+      MSAMXMSMSA
+      AMXSXMAAMM
+      MSAMASMSMX
+      XMASAMXAMM
+      XXAMMXXAMA
+      SMSMSASXSS
+      SAXAMASAAA
+      MAMMMXMMMM
+      MXMXAXMASX
+    """
+        .trimIndent()
+        .let { input ->
+          //          assertEquals(null, part1(input))
+          assertEquals(9, part2(input))
+        }
+  }
+}
