c5767290ec7182f44491e90bd32411960cff8cf0
Dan Zou <danzou193@gmail.com>
2024-12-05 10:27:08 -0500

    Implement first pass 2024-5

---
+++ src/main/kotlin/dev/danzou/advent24/Day5.kt
@@ -0,0 +1,108 @@
+package dev.danzou.advent24
+
+import dev.danzou.advent.utils.*
+import dev.danzou.advent.utils.geometry.toPair
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class Day5 : AdventTestRunner24("") {
+  fun parseRules(input: String): Map<Pair<Int, Int>, Pair<Int, Int>> =
+      input
+          .lines()
+          .takeWhile { it != "" }
+          .map { it.split("|").map(String::toInt) }
+          .flatMap { (i1, i2) ->
+            listOf(
+                (i1 to i2) to (i1 to i2),
+                (i2 to i1) to (i1 to i2),
+            )
+          }
+          .toMap()
+
+  fun parseUpdates(input: String): List<List<Int>> =
+      input
+          .lines()
+          .dropWhile { it != "" }
+          .drop(1)
+          .map { it.split(",").map(String::toInt) }
+
+  override fun part1(input: String): Any {
+    val orderingRules = parseRules(input)
+    val updates = parseUpdates(input)
+    return updates
+        .filter {
+          it.pairs()
+              .map { it.toPair() }
+              .toSet()
+              .all { pair ->
+                if (pair in orderingRules)
+                    it.indexOf(orderingRules[pair]!!.first) <
+                        it.indexOf(orderingRules[pair]!!.second)
+                else true
+              }
+        }
+        .sumOf { it[it.size / 2] }
+  }
+
+  override fun part2(input: String): Any {
+    val orderingRules = parseRules(input)
+    val updates = parseUpdates(input)
+    val rulesSort: (Int, Int) -> Int = { i1, i2 ->
+      val rule = orderingRules[i1 to i2]!!
+      if (rule.first == i1) -1 else 1
+    }
+    return updates
+        .filter {
+          !(it.pairs()
+              .map { it.toPair() }
+              .toSet()
+              .all { pair ->
+                if (pair in orderingRules)
+                    it.indexOf(orderingRules[pair]!!.first) <
+                        it.indexOf(orderingRules[pair]!!.second)
+                else true
+              })
+        }
+        .map { it.sortedWith(rulesSort) }
+        .sumOf { it[it.size / 2] }
+  }
+
+  @Test
+  fun testExample() {
+    """
+      47|53
+      97|13
+      97|61
+      97|47
+      75|29
+      61|13
+      75|53
+      29|13
+      97|29
+      53|29
+      61|53
+      97|53
+      61|29
+      47|13
+      75|47
+      97|75
+      47|61
+      75|61
+      47|29
+      75|13
+      53|13
+
+      75,47,61,53,29
+      97,61,53,29,13
+      75,29,13
+      75,97,47,61,53
+      61,13,29
+      97,13,75,29,47
+    """
+        .trimIndent()
+        .let { input ->
+          assertEquals(143, part1(input))
+          assertEquals(123, part2(input))
+        }
+  }
+}
