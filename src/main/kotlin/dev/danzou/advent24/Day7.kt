f95e3f0765ab33a86291667bf74562ceb601a582
Dan Zou <danzou193@gmail.com>
2024-12-07 00:36:44 -0500

    haha one function

--- src/main/kotlin/dev/danzou/advent24/Day7.kt
+++ src/main/kotlin/dev/danzou/advent24/Day7.kt
@@ -28,23 +28,19 @@ internal class Day7 : AdventTestRunner24("") {
     return step(nums.first(), nums.subList(1, nums.size))
   }
 
-  override fun part1(input: String): Long {
+  fun day7(input: String, ops: List<(Long, Long) -> Long>): Long {
     val parsed = getEquations(input)
-    val ops: List<(Long, Long) -> Long> = listOf(Long::plus, Long::times)
-
     return parsed
         .filter { (target, nums) -> isSolvable(target, nums, ops) }
         .sumOf { (target, _) -> target }
   }
 
-  override fun part2(input: String): Long {
-    val parsed = getEquations(input)
-    val ops: List<(Long, Long) -> Long> =
-        listOf(Long::plus, Long::times, { l1, l2 -> "$l1$l2".toLong() })
+  override fun part1(input: String): Long {
+    return day7(input, listOf(Long::plus, Long::times))
+  }
 
-    return parsed
-        .filter { (target, nums) -> isSolvable(target, nums, ops) }
-        .sumOf { (target, _) -> target }
+  override fun part2(input: String): Long {
+    return day7(input, listOf(Long::plus, Long::times, { l1, l2 -> "$l1$l2".toLong() }))
   }
 
   @Test
