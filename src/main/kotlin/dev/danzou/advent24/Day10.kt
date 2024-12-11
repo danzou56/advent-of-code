0bfe3037a83aa90269a8c123e566b46878d78019
Dan Zou <danzou193@gmail.com>
2024-12-10 21:31:53 -0500

    Implement 2024-10

---
+++ src/main/kotlin/dev/danzou/advent24/Day10.kt
@@ -0,0 +1,61 @@
+package dev.danzou.advent24
+
+import dev.danzou.advent.utils.*
+import java.util.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class Day10 : AdventTestRunner24("") {
+  override fun part1(input: String): Int {
+    val map = input.asMatrix<Int>()
+
+    val starts = map.indices2D.filter { map[it] == 0 }
+
+    return starts
+        .map { start ->
+          dfs(start) { pos ->
+            map.neighboringPos(pos).filter { map[it] == map[pos] + 1 }.toSet()
+          }
+        }
+        .sumOf { it.count { map[it] == 9 } }
+  }
+
+  override fun part2(input: String): Int {
+    val map = input.asMatrix<Int>()
+    val starts = map.indices2D.filter { map[it] == 0 }
+
+    return starts
+        .flatMap { start ->
+          dfs(start) { pos ->
+                map.neighboringPos(pos).filter { map[it] == map[pos] + 1 }.toSet()
+              }
+              .filter { map[it] == 9 }
+              .map { start to it }
+        }
+        .flatMap { (start, end) ->
+          findPaths(start, end) { pos ->
+            map.neighboringPos(pos).filter { map[it] == map[pos] + 1 }.toSet()
+          }
+        }
+        .size
+  }
+
+  @Test
+  fun testExample() {
+    """
+      89010123
+      78121874
+      87430965
+      96549874
+      45678903
+      32019012
+      01329801
+      10456732
+    """
+        .trimIndent()
+        .let { input ->
+          assertEquals(36, part1(input))
+          // assertEquals(null, part2(input))
+        }
+  }
+}
