a57dea156d6cec12bda2282597249fe23ba952ee
Dan Zou <danzou193@gmail.com>
2025-12-01 13:08:05 -0500

    Implement 2025-01

---
+++ src/main/kotlin/dev/danzou/advent25/AdventTestRunner25.kt
@@ -0,0 +1,5 @@
+package dev.danzou.advent25
+
+import dev.danzou.advent.utils.AdventTestRunner
+
+abstract class AdventTestRunner25(name: String) : AdventTestRunner(25, name)
---
+++ src/main/kotlin/dev/danzou/advent25/Day1.kt
@@ -0,0 +1,63 @@
+package dev.danzou.advent25
+
+import dev.danzou.advent.utils.*
+import kotlin.text.split
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class Day1 : AdventTestRunner25("Secret Entrance") {
+  val size = 100
+  val start = 50
+
+  private fun parse(input: String): List<Int> =
+      input.split("\n").map {
+        when (it.take(1)) {
+          "L" -> -1
+          else -> 1
+        } * it.drop(1).toInt()
+      }
+
+  override fun part1(input: String): Int =
+      parse(input)
+          .fold(Pair(start, 0)) { (pos, pass), rot ->
+            val next = ((pos + rot).mod(size) + size).mod(size)
+            if (next == 0) Pair(next, pass + 1) else Pair(next, pass)
+          }
+          .second
+
+  override fun part2(input: String): Int =
+      parse(input)
+          .fold(Pair(start, 0)) { (pos, pass), rot ->
+            val zeroes =
+                if (rot > 0) {
+                  (pos + rot) / size
+                } else {
+                  (size - pos - rot) / size + if (pos == 0) -1 else 0
+                }
+
+            val next = ((pos + rot).mod(size) + size).mod(size)
+            Pair(next, pass + zeroes)
+          }
+          .second
+
+  @Test
+  fun testExample() {
+    """
+      L68
+      L30
+      R48
+      L5
+      R60
+      L55
+      L1
+      L99
+      R14
+      L82
+    """
+        .trimIndent()
+        .let { input ->
+          assertEquals(3, part1(input))
+          assertEquals(6, part2(input))
+        }
+  }
+}
