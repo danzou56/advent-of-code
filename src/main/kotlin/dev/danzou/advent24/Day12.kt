266a454508f4d64a360be1bf7a2febf6c095e052
Dan Zou <danzou193@gmail.com>
2025-12-02 19:40:49 -0500

    Cleanup 24-12

--- src/main/kotlin/dev/danzou/advent24/Day12.kt
+++ src/main/kotlin/dev/danzou/advent24/Day12.kt
@@ -11,14 +11,14 @@ internal class Day12 : AdventTestRunner24("Garden Groups") {
   tailrec fun divideRegions(
       garden: Matrix<Char>,
       remaining: Set<Pos>,
-      discovered: List<Set<Pos>> = emptyList()
+      discovered: List<Set<Pos>> = emptyList(),
   ): List<Set<Pos>> {
     if (remaining.isEmpty()) return discovered
     val start = remaining.first()
     val target = garden[start]
     val curPlot =
-        dfs(start) {
-          garden.neighboringPos(it).filter { garden[it] == target }.toSet()
+        dfs(start) { pos ->
+          garden.neighboringPos(pos).filter { adj -> garden[adj] == target }.toSet()
         }
     val nextRemaining = remaining - curPlot
     val nextDiscovered = discovered + listOf(curPlot)
@@ -31,31 +31,23 @@ internal class Day12 : AdventTestRunner24("Garden Groups") {
         garden.indices2D
             .groupBy { garden[it] }
             .flatMap { (plotChar, plotPos) ->
-              divideRegions(garden, plotPos.toSet()).map {
-                plotChar to it
-              }
+              divideRegions(garden, plotPos.toSet()).map { plotChar to it }
             }
 
-    return plots
-        .map { (plotChar, posList) ->
-          val area = posList.size
-          val perimeter =
-              posList.sumOf { pos ->
-                CARDINAL_DIRECTIONS.count { dir ->
-                  val adj = pos + dir
-                  !garden.containsPos(adj) || garden[adj] != plotChar
-                }
-              }
-          area * perimeter
-        }
-        .sum()
+    return plots.sumOf { (plotChar, posList) ->
+      val area = posList.size
+      val perimeter =
+          posList.sumOf { pos ->
+            CARDINAL_DIRECTIONS.count { dir ->
+              val adj = pos + dir
+              !garden.containsPos(adj) || garden[adj] != plotChar
+            }
+          }
+      area * perimeter
+    }
   }
 
-  tailrec fun countSides(
-      garden: Matrix<Char>,
-      remaining: Set<Pos>,
-      count: Int = 0
-  ): Int {
+  tailrec fun countSides(garden: Matrix<Char>, remaining: Set<Pos>, count: Int = 0): Int {
     if (remaining.isEmpty()) return count
     val start = remaining.first()
     val curSide =
@@ -70,16 +62,15 @@ internal class Day12 : AdventTestRunner24("Garden Groups") {
         garden.indices2D
             .groupBy { garden[it] }
             .flatMap { (plotChar, posList) ->
-              divideRegions(garden, posList.toSet()).map {
-                plotChar to it
-              }
+              divideRegions(garden, posList.toSet()).map { plotChar to it }
             }
 
     return plots.sumOf { (plotChar, posList) ->
       val area = posList.size
       val sides =
           posList
-              // Find all perimeter positions - since we need to find "sides", we also need to keep
+              // Find all perimeter positions - since we need to find "sides", we also
+              // need to keep
               // the direction in order to later count and disambiguate disjoint sides
               .flatMap { pos ->
                 CARDINAL_DIRECTIONS.filter { dir ->
@@ -89,15 +80,18 @@ internal class Day12 : AdventTestRunner24("Garden Groups") {
                     .map { dir -> pos to dir }
               }
               // Group by side (sort of) - pos/dir pairs on the same side share the same y
-              // coordinate for north/south sides and share the same x coordinate for east/west
+              // coordinate for north/south sides and share the same x coordinate for
+              // east/west
               // sides
               .groupBy { (pos, dir) -> if (dir.x == 0) pos.y to dir else pos.x to dir }
               .values
               .sumOf { posDirList: List<Pair<Pos, Pos>> ->
-                // At this point, the direction the side faces is no longer relevant and can be
+                // At this point, the direction the side faces is no longer relevant and
+                // can be
                 // thrown away
                 val posSet = posDirList.map { (pos, _) -> pos }.toSet()
-                // Grouping by pos.y/x,dir will produce scenarios where disjointed sides are in the
+                // Grouping by pos.y/x,dir will produce scenarios where disjointed sides
+                // are in the
                 // same group so use a search to disambiguate the sides
                 countSides(garden, posSet)
               }
