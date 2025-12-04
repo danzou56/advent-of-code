952a40b04db526a8e01599b13505562855b9d2c0
Dan Zou <danzou193@gmail.com>
2025-12-04 13:35:25 -0500

    Reformat

--- src/main/kotlin/dev/danzou/advent25/Day4.kt
+++ src/main/kotlin/dev/danzou/advent25/Day4.kt
@@ -5,12 +5,15 @@ import dev.danzou.advent.utils.geometry.Compass.Companion.ALL_DIRECTIONS
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-internal class Day4 : AdventTestRunner25("") {
+internal class Day4 : AdventTestRunner25("Printing Department") {
+  private val PAPER = '@'
+  private val EMPTY = '.'
+
   fun getAccessible(map: Matrix<Char>): Collection<Pos> =
-    map.indices2D
-          .filter { map[it] == '@' }
+      map.indices2D
+          .filter { map[it] == PAPER }
           .filter { pos ->
-            map.neighboringPos(pos, ALL_DIRECTIONS).count { map[it] == '@' } < 4
+            map.neighboringPos(pos, ALL_DIRECTIONS).count { map[it] == PAPER } < 4
           }
 
   override fun part1(input: String): Int = getAccessible(input.asMatrix<Char>()).size
@@ -21,7 +24,7 @@ internal class Day4 : AdventTestRunner25("") {
       val accessible = getAccessible(map)
       if (accessible.isEmpty()) return 0
       return accessible.size +
-          step(map.mapIndexed2D { pos, el -> if (pos in accessible) '.' else el })
+          step(map.mapIndexed2D { pos, el -> if (pos in accessible) EMPTY else el })
     }
 
     return step(input.asMatrix<Char>())
