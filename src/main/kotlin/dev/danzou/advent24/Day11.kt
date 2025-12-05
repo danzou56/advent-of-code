5f1894c179b66c89d7e24da86286ba8f63a7e76e
Dan Zou <danzou193@gmail.com>
2025-12-05 17:34:17 -0500

    Make 2024-11 not slow

--- src/main/kotlin/dev/danzou/advent24/Day11.kt
+++ src/main/kotlin/dev/danzou/advent24/Day11.kt
@@ -1,46 +1,34 @@
 package dev.danzou.advent24
 
-import dev.danzou.advent.utils.frequencyMap
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal class Day11 : AdventTestRunner24("Plutonian Pebbles") {
 
   data class Stone(val id: Long) {
-    fun blink(): List<Stone> {
-      return if (id == 0L) OneStone
-      else if (id.toString().length % 2 == 0) {
-        val digits = id.toString()
-        listOf(
-            Stone(digits.take(digits.length / 2).toLong()),
-            Stone(digits.drop(digits.length / 2).toLong()))
-      } else listOf(Stone(id * 2024))
-    }
-
-    companion object {
-      val OneStone = listOf(Stone(1))
-    }
+    fun blink(): List<Stone> =
+        id.toString().let { sId ->
+          when {
+            sId == "0" -> listOf(Stone(1))
+            sId.length % 2 != 0 -> listOf(Stone(id * 2024))
+            else -> {
+              listOf(
+                  Stone(sId.take(sId.length / 2).toLong()),
+                  Stone(sId.drop(sId.length / 2).toLong()),
+              )
+            }
+          }
+        }
   }
 
-  fun blink(stones: Map<Stone, Long>, times: Int): Map<Stone, Long> {
+  tailrec fun blink(stones: Map<Stone, Long>, times: Int): Map<Stone, Long> {
+    if (times == 0) return stones
 
-    val mut = stones.toMutableMap()
-
-    tailrec fun step(stones: Map<Stone, Long>, times: Int): Map<Stone, Long> {
-      if (times == 0) return stones
-
-      val next =
-          stones
-              .map { (stone, count) ->
-                stone.blink().frequencyMap().mapValues { (_, v) -> v * count }
-              }
-              .fold(emptyMap<Stone, Long>()) { acc, cur ->
-                (acc.keys + cur.keys).associateWith { (acc[it] ?: 0) + (cur[it] ?: 0) }
-              }
-      return step(next, times - 1)
-    }
-
-    return step(stones, times)
+    val next = mutableMapOf<Stone, Long>()
+    stones
+        .flatMap { (stone, count) -> stone.blink().map { s -> s to count } }
+        .forEach { (stone, count) -> next[stone] = (next[stone] ?: 0) + count }
+    return blink(next, times - 1)
   }
 
   override fun part1(input: String): Long {
